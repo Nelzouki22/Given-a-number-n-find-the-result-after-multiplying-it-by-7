6cb0de9f5f80f8e8abb46b9c9ab3fc3d05b83f6e
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-05-25 21:16:50 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    int result = n * 7;
+
+    std::cout << "The result after multiplying " << n << " by 7 is: " << result << std::endl;
+
+    return 0;
+}
